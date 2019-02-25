774ddaa813c1a2107ee858b3b696b58734f4ee64
18320867985 <18320867985>
2019-02-25 11:11:20 +0800

    2018-2-25

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,7 +2,7 @@
 const router = require("./_router");
 
 // filter
-//require("./filter");
+require("./filter");
 
 router.get("/index",(req,res)=>{
     res.render("admin/index",{});
@@ -43,7 +43,6 @@ require("./contact");
 
 // svcnet
 require("./svcnet");
-
 
 // article
 require("./article");
